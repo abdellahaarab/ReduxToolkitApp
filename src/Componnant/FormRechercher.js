b8970821485b1a6637478a030d31f9dc3568cbb9
Abdellah Aarab <105945475+abdellahaarab@users.noreply.github.com>
2023-01-15 22:43:33 +0100

    initial commit in branch _dev

--- src/Componnant/FormRechercher.js
+++ src/Componnant/FormRechercher.js
@@ -4,17 +4,38 @@ import { useSelector } from 'react-redux'
 function FormRechercher() {
   const data = useSelector(state=>state.stagere.stageres)
   const [Serch, setSerch] = useState('')
+  const [Resulte, setResulte ] = useState(null)
 
   function fRes(e){
     e.preventDefault();
-    // console.log(data)
     const resulte = data.find((v) => v.nomeESTprenome === Serch)
-    console.log("Data : ", resulte)
+    setResulte(resulte)
   }
   return (
     <div>
         <input type="text" onChange={(e)=>setSerch(e.target.value)} placeholder='Something to start ...'/> 
         <button onClick={fRes}>Rechercher</button>
+        <span >
+             {
+                Resulte ?
+                  
+                <div style={{display:"flex"}}>
+                          <div>-Numero : {Resulte.numero } - </div> 
+                          <div>Nome : {Resulte.nomeESTprenome } - </div> 
+                          <div>Fileire : {Resulte.fileire } </div> 
+                          
+                          <ol>                     
+                             {
+                                Resulte.notes.map((e,i)=>< li key={i}>Module : {e.module} - Moyane Module : {e.moyanmodule}</li>)
+                            }
+                          </ol>
+                          
+                           
+                </div> 
+                :
+                <p>There's no data </p>
+             }
+        </span>
     </div>
   )
 }
